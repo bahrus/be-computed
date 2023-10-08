8f73410f6c57ca0afb5ec1c76cc21266e509112d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-10-08 18:10:59 -0400

    Initial commit

---
+++ prsFrom.js
@@ -0,0 +1,44 @@
+import { tryParse } from 'be-enhanced/cpu.js';
+import { lispToCamel } from 'trans-render/lib/lispToCamel.js';
+const reValueStatement = [
+    {
+        regExp: new RegExp(String.raw `^(?<dependencies>.*)`),
+        defaultVals: {}
+    }
+];
+export function prsFrom(self) {
+    //be careful about making this asynchronous due to instructions getting out of sync
+    let { from, instructions } = self;
+    if (instructions === undefined)
+        instructions = [];
+    const args = [];
+    const instruction = {
+        args
+    };
+    instructions.push(instruction);
+    const val0 = from[0];
+    const test = tryParse(val0, reValueStatement);
+    if (test === null)
+        throw 'PE'; //Parse Error
+    const { dependencies } = test;
+    const splitDependencies = dependencies.split(',').map(x => x.trim());
+    for (const dependency of splitDependencies) {
+        const type = dependency[0];
+        let prop = dependency.substring(1);
+        let attr = undefined;
+        if (type === '-') {
+            attr = '-' + prop;
+            prop = lispToCamel(prop);
+        }
+        const arg = {
+            type,
+            prop,
+            attr,
+        };
+        args.push(arg);
+    }
+    //console.log({test, splitDependencies});
+    return {
+        instructions
+    };
+}
